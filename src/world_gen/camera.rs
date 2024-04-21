05bb87870d6d73cd97c0b4dc825cf7245f1dc474
bentles <dee.bent.lee@gmail.com>
2024-04-21 17:06:43 +0200

    nicer direction change checking

--- src/world_gen/camera.rs
+++ src/world_gen/camera.rs
@@ -26,33 +26,9 @@ pub fn detect_camera_direction_changed(
         let facing: Direction3d = transform.forward();
         past_cam_dir.0 = facing;
 
-        //matrices???
-        let x = Vec3 {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-        };
-        let r1 = was_facing.dot(x);
-        let r2 = facing.dot(x);
-        let x_changed = r1 * r2 < 0.0;
-
-        let y = Vec3 {
-            x: 0.0,
-            y: 1.0,
-            z: 0.0,
-        };
-        let u1 = was_facing.dot(y);
-        let u2 = facing.dot(y);
-        let y_changed = u1 * u2 < 0.0;
-
-        let z = Vec3 {
-            x: 0.0,
-            y: 0.0,
-            z: 1.0,
-        };
-        let f1 = was_facing.dot(z);
-        let f2 = facing.dot(z);
-        let z_changed = f1 * f2 < 0.0;
+        let x_changed = was_facing.dot(Vec3::X) * facing.dot(Vec3::X) < 0.0;
+        let y_changed = was_facing.dot(Vec3::Y) * facing.dot(Vec3::Y) < 0.0;
+        let z_changed = was_facing.dot(Vec3::Z) * facing.dot(Vec3::Z) < 0.0;
 
         if x_changed || y_changed || z_changed {
             ev_levelup.send(CameraDirectionChangeEvent);
